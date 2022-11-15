03668a6d42faf0426fec8619e1c9fd6a8630ce3c
Hoxxus <waldemarschigal@yahoo.de>
2022-11-15 20:49:02 +0100

    Exercise finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringToArray = string.split("");
+    const reversedArray = stringToArray.reverse();
+    return reversedArray.join("");
+    // return string.split("").reverse().join(""); Kann in eine Zeile zusammengefasst werden.
 };
 
 // Do not edit below this line
