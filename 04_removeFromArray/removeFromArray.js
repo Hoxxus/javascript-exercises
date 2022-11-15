97a1a678462b877a32e8cb18d1ba756913365bef
Hoxxus <waldemarschigal@yahoo.de>
2022-11-15 21:40:11 +0100

    Exercise finished.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removeArgs) {
+    let reducedArray = [];
 
+    for (const inputElement of inputArray) {
+        let relevantElement = true;
+
+        for (let i = 0; i < removeArgs.length; i++) {
+            if (inputElement === removeArgs[i]) relevantElement = false;
+        }
+        
+        if (relevantElement) {
+            reducedArray.push(inputElement);
+            relevantElement = true;
+        }
+    }
+    return reducedArray; 
 };
 
 // Do not edit below this line
