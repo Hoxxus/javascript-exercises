aee2b6aeab45d2ad13b854cbd7ded12ecb5e6be0
Hoxxus <waldemarschigal@yahoo.de>
2022-11-15 20:33:45 +0100

    Exercise finished.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+  
+  let returnString = "";
+  
+  if (n < 0) {
+    return "ERROR";
+  } else {
+    while (n--) {
+      returnString += string;
+    }
+    return returnString;
+  }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
