6e88a6c4928e3e4926c56038908f61ff7ce90185
Hoxxus <waldemarschigal@yahoo.de>
2022-11-16 15:23:33 +0100

    Exercise finished.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  
+  const numbers = [a, b];
+  numbers.sort(function (x, y) {return x-y});
 
+  let sum = 0;
+  for (let i = numbers[0]; i <= numbers[1]; i++) {
+    sum += i;    
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
