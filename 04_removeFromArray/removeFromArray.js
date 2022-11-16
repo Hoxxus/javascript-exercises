dc32595c156114e4e10ea5926aed548463a85aec
Hoxxus <waldemarschigal@yahoo.de>
2022-11-16 10:32:15 +0100

    Practices to adjust code with includes() and filter()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,46 @@
-const removeFromArray = function(inputArray, ...removeArgs) {
-    let reducedArray = [];
+// const removeFromArray = function(inputArray, ...removeArgs) {
+//     const reducedArray = [];
 
-    for (const inputElement of inputArray) {
-        let relevantElement = true;
+//     for (const inputElement of inputArray) {
+//         let relevantElement = true;
 
-        for (let i = 0; i < removeArgs.length; i++) {
-            if (inputElement === removeArgs[i]) relevantElement = false;
-        }
+//         for (let i = 0; i < removeArgs.length; i++) {
+//             if (inputElement === removeArgs[i]) relevantElement = false;
+//         }
         
-        if (relevantElement) {
-            reducedArray.push(inputElement);
-            relevantElement = true;
-        }
-    }
-    return reducedArray; 
+//         if (relevantElement) {
+//             reducedArray.push(inputElement);
+//             relevantElement = true;
+//         }
+//     }
+//     return reducedArray; 
+// };
+
+// // Do not edit below this line
+// module.exports = removeFromArray;
+
+//#####################################################################
+
+// kürzerer Code mit includes() Methode
+
+// const removeFromArray = function(inputArray, ...removeArgs) {
+//     const reducedArray = [];
+//     for (const inputElement of inputArray) {
+//         if (!removeArgs.includes(inputElement)) {
+//             reducedArray.push(inputElement);
+//         }
+//     }
+//     return reducedArray; 
+// };
+
+// module.exports = removeFromArray;
+
+//#####################################################################
+
+// noch kürzerer Code mit filter() und includes()
+
+const removeFromArray = function(inputArray, ...removeArgs) {
+    return inputArray.filter(val => !removeArgs.includes(val));
 };
 
-// Do not edit below this line
 module.exports = removeFromArray;
