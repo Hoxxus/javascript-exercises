ac70d0045680fc6e98ecbd667107fdbc2b4c4924
Hoxxus <waldemarschigal@yahoo.de>
2022-11-22 22:35:14 +0100

    Finished Exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+  const oldest = peeps.sort((a, b) => {
+    const currentYear = (new Date()).getFullYear();
+    return ((a.yearOfDeath ?? currentYear) - a.yearOfBirth) - ((b.yearOfDeath ?? currentYear) - b.yearOfBirth);
+  });
 
-};
+  return oldest[oldest.length - 1];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
