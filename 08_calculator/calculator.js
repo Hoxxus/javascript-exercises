cc5732765723e0d74088678ce63cb34ee7c9674e
Hoxxus <waldemarschigal@yahoo.de>
2022-11-22 21:08:13 +0100

    Finished exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	let sum = 0;
+  for(const num of numbers) {
+    sum += Number(num);
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  let sub = Number(numbers[0]);
+  for(let i = 1; i < numbers.length; i++) {
+    sub = sub - Number(numbers[i]);
+  }
+  return sub;
 };
 
-const sum = function() {
-	
+
+
+const sum = function(numbers) {
+	let sum = 0;
+  for(const num of numbers) {
+    sum += Number(num);
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	let mult = 1;
+  for(const num of numbers) {
+    mult = mult * Number(num);
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {return 1};  
+  let fact = num;
+	for (let i = 1; i < num ; i++) {
+     fact = fact * (num - i);
+  }
+  return fact;
 };
 
 // Do not edit below this line
