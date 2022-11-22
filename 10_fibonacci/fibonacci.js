2cfcee6ae3509b47128d3cda9420b4ad3c2bca9e
Hoxxus <waldemarschigal@yahoo.de>
2022-11-22 22:03:18 +0100

    Finished exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  num = Number(num);
+  if (num < 0) {return "OOPS"}
 
+  const fib = [1, 1,];
+  const counter = num - 2;
+  for (let i = 0; i < counter; i++) {
+    fib.push(fib[i] + fib[i+1]);
+  }
+
+  return fib[num-1];
 };
 
 // Do not edit below this line
