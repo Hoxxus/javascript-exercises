feeb00a43cd5a9eec8f1dc51b2fa6fa75d03affc
Hoxxus <waldemarschigal@yahoo.de>
2022-11-22 21:46:47 +0100

    Finished exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+  const regex = /[A-Za-z]+/g;
+  const arr = [...phrase.matchAll(regex)];
+  const pali = arr.join("").toLowerCase();
+  const paliReverse = pali.split("").reverse().join("");
 
+  return (pali === paliReverse); 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
